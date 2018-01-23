8091e039555f85062a212cd369562b3be64ed185
skconan <supakit.kr@gmail.com>
2018-01-23 11:20:35 +0700

    rewrite

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -12,38 +12,30 @@ p = Print(debug)
 class Preprocessing:
     def __init__(self):
         self.input = []
+        self.output = []
         pass
 
-    def preprocessing(self, filename, col=1):
+    def preprocessing(self, filename):
         p.print('==== preprocessing oil gland feature ====')
         f = open(filename, 'r')
-        if col == 1:
-            while True:
-                row = f.readline()
-                if row is '':
-                    break
-                self.input.append(float(row))
-                
-            self.input = preprocessing.normalize(self.input)[0]
-            self.input = self.input[:,np.newaxis]
-        elif col == 3:
-            while True:
-                row = f.readline()
-                if row is '':
-                    break
-                row = row.split(',')
-                self.input.append([float(row[0]),float(row[1]),float(row[2])])
-            self.input = np.array(self.input)
-            self.input = preprocessing.normalize(self.input)
+        row = f.readline()
+        
+        while True:
+            row = f.readline()
+            if row is '':
+                break
+            row = row.split(',')
+            self.input.append([float(row[0]),float(row[1]),float(row[2])])
+            self.output.append(int(row[4]))
+        self.input = np.array(self.input)
+        self.input = preprocessing.normalize(self.input)
         p.print(self.input)
-        # return self.input
-
 
     def get_input(self):
         return self.input
 
     def get_output(self):
-        return list(range(1,len(self.input)+1,1))
+        return self.output
 
 
 
@@ -51,6 +43,7 @@ class Preprocessing:
 if __name__ == '__main__':
     # p.change_mode(True)
     pp = Preprocessing()
-    pp.preprocessing('histogram_A1',3)
+    pp.preprocessing('Pomelo_features_B')
     print(pp.get_input())
+    print(pp.get_output())
     pass
