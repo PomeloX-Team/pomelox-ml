c79483d53ef45cedb1b4ea826e7d5d64eec1b91f
skconan <supakit.kr@gmail.com>
2018-02-17 17:42:05 +0700

    refactor code

--- draw_circle.py
+++ draw_circle.py
@@ -1,7 +1,14 @@
+'''
+    File name: draw_circle.py
+    Author: PomeloX
+    Date created: 2/1/2018
+    Date last modified: 2/17/2018
+    Python Version: 3.6.1
+'''
+
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 from lib import *
+import numpy as np
 import constant as CONST
 from operator import itemgetter
 
@@ -9,7 +16,6 @@ debug = False
 p = Print(debug)
 
 def circle_matching(mask_cir,mask,x,y,r):
-    global p
     found = 0
     for i in range(x-r-5,x+r+5):
         for j in range(y-r-5,y+r+5):
@@ -19,129 +25,102 @@ def circle_matching(mask_cir,mask,x,y,r):
 
 def draw_circle(image):
     global p
-    image_default = image.copy()
-    img_size = 300
-    centroid = int(img_size / 2)
-    r_min = 80
-    r_max = 100
-    centroid_range = range(centroid-15,centroid+15)
-
-    blur = cv2.medianBlur(image,7)
-    clahe = clahe_by_Lab(blur)
-    image = clahe
-
     result = image.copy()
     row, col, ch = image.shape
 
+    img_size = 100
+    centroid = int(img_size / 2)
+    r_min = 25
+    r_max = 35
+    centroid_range = range(centroid-5,centroid+5)
+
+    blur = cv2.medianBlur(image,3)
+    clahe = clahe_by_Lab(blur)
+    image = clahe
 
     img = cv2.resize(image,(img_size,img_size))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
-    result_s = img.copy()
     row, col, ch = img.shape
-    mask = np.uint8(np.zeros((row, col)))
 
     h, s, v = cv2.split(hsv) 
-
-    
     h_mode = get_mode(h)
-    print(h_mode)
-    if h_mode is None:
-        h_mode = 37
-    print(h_mode)
+    
     lower_bound = np.array([h_mode - 12, 0, 0], dtype=np.uint8)
     upper_bound = np.array([h_mode + 12, 255, 255], dtype=np.uint8)
     res_inrange = cv2.inRange(hsv, lower_bound, upper_bound)
     res_inrange_inv = 255 - res_inrange
+
     kernel = get_kernel('rect',(3,3))
     res_inrange_inv = cv2.dilate(res_inrange_inv, kernel, iterations=1)   
-    result_matching = []
-   
     
+    result_matching = []
     for x in centroid_range:
         for y in centroid_range:
             for r in range(r_min,r_max+1):
                 center = (x,y)
-                mask_cir = mask.copy()
-                cv2.circle(mask_cir, center, r, (255), 4)
+                mask_cir = np.uint8(np.zeros((row, col)))
+                cv2.circle(mask_cir, center, r, (255), 2)
                 number_of_match = circle_matching(mask_cir,res_inrange_inv,x,y,r)
                 result_matching.append([number_of_match,center,r])
         print(x)
                 
     result_matching = sorted(result_matching, key=itemgetter(0),reverse=True)
-    print(result_matching[:1])
+    print(result_matching[:5])
+    
     x_mean, y_mean, r_mean = [],[],[]
-    for res in result_matching[:1]:
+    for res in result_matching[:5]:
         _,center,r = res
         x,y = center
-        x_mean.append(x)
-        y_mean.append(y)
-        r_mean.append(r)
-        cv2.circle(result_s, center, r, (255, 0, 255), 1)
-        cv2.circle(image_default, (x*2,y*2), r*2, (255, 0, 255), 3)
-        cv2.circle(res_inrange_inv, center, r, (155, 155, 155), 1)
-
-
+        cv2.circle(result, (x*6,y*6), r*6, (255, 255, 255), 6)
+        cv2.circle(res_inrange_inv, center, r, (155, 155, 155), 2)
 
     p.imshow('res_inrange', res_inrange_inv)
-    p.imshow('result', result)
-    p.imshow('mask', mask)
-    p.imshow('image', image)
-    # p.imshow('img_hsv', hsv)
+    p.imshow('image', result)
+
     k = cv2.waitKey(0)
     if k == ord('e'):
         exit(0)
-   
-    
+       
     if not p.get_mode():
-        return image_default
+        return result
 
 
 def main():
-    global p
-    symbol = []
-    print('Put number of symbol: ')
-    n_symbol = input()
-    for i in range(0,int(n_symbol),1):
-        print('Put symbol #',i,' : ')
-        s = input()
-        print('Put order of symbol ex. 1-5: ')
-        o = input()
-        o1,o2 = o.split('-')
-        o1 = int(o1)
-        o2 = int(o2)
-        for j in range(o1,o2+1,1):
-            symbol.append(s+str(j))
-    print(symbol)
-    error_list = []
+    print('>' * 10, 'Draw circle on the image(s)', '<' * 10, '\n')
+    symbol_list = get_symbol_list()
+
     date = gen_date()
-    print('Processing...')
-    ct = 0
-    k = 'y'
-    for s in symbol:
+    overwrite = False
+
+    for s in symbol_list:
         for j in date:
             img_name = s + '_' + j + '.JPG'
-            img_name = 'A1_20171112.JPG'
-            if ct == 0 and not cv2.imread(CONST.IMG_CIRCLE_PATH + img_name, 1) is None:
-                print('There is already file [press N or n] to exit or [any key] to continue.')
-                k = input()
-                ct += 1
-            
-            if k == 'N' or k == 'n':
-                exit(0)
-            print(img_name)
+            print(img_name, 'is processing...\n')
             img = cv2.imread(CONST.IMG_RESIZE_PATH + img_name, 1)
+
+            if img is None:
+                print('Cannot read image: ', img_name)
+                continue
+
             res = draw_circle(img)
 
-            # if not p.get_mode():
-            #     continue
-
             if res is None:
-                error_list.append(img_name)
+                print('Cannot draw circle on image: ', img_name)
                 continue
-            cv2.imwrite(CONST.IMG_CIRCLE_PATH + img_name, res)
-    p.print(error_list)
 
+            if (not overwrite) and (not cv2.imread(CONST.IMG_CIRCLE_PATH + img_name, 1) is None):
+                print('There is already a file with the same name\nPress s or S to Skip\nPress o or O to Overwrite\nPress a or A to Overwrite to all\nPress e or E to exit program\n')
+                k = input()
+                k = k.lower()
+                if k == 's':
+                    continue
+                elif k == 'a':
+                    overwrite = True
+                elif k == 'e':
+                    break
+
+            cv2.imwrite(CONST.IMG_CIRCLE_PATH + img_name, res)
 
 if __name__ == '__main__':
     # p.change_mode(True)
