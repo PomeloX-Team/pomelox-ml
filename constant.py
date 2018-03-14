73948fde206636dd5d7e9644e352729746640e6c
skconan <supakit.kr@gmail.com>
2018-03-14 21:30:29 +0700

    edit width of image

--- constant.py
+++ constant.py
@@ -2,5 +2,5 @@ IMG_PATH = 'images/'
 IMG_OIL_GLAND_PATH = 'images_oil_gland/'
 IMG_RESIZE_PATH = 'images_resize/'
 IMG_CIRCLE_PATH = 'images_circle/'
-RESULT_WIDTH = 600
-RESULT_HEIGHT = 600
+RESULT_WIDTH = 1600
+RESULT_HEIGHT = 1600
