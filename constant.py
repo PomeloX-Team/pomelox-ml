c9326a8378c46e3bb57d9eaf18301a22f2b417bf
skconan <supakit.kr@gmail.com>
2018-03-28 01:15:20 +0700

    update constant value

--- constant.py
+++ constant.py
@@ -1,7 +1,7 @@
 IMG_PATH = 'images/'
 IMG_OIL_GLAND_PATH = 'images_oil_gland/'
 IMG_RESIZE_PATH = 'images_resize/'
-IMG_CIRCLE_PATH = 'images_circle/'
+IMG_CIRCLE_PATH = 'images_rect/'
 RESULT_WIDTH = 1600
 RESULT_HEIGHT = 1600
-RECT_SIZE = 600
+RECT_SIZE = 300
